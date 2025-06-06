a71f7c8a860f721a7eaddb875fed87dc904c4f48
pretend_alphs <abidkasu@gmail.com>
2025-06-06 19:10:16 +0530

    Add HomeController and index.html for basic routing and homepage

---
+++ src/main/java/com/one/eCommerce_backend/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.one.eCommerce_backend.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+
+    @RequestMapping("/hello")
+    public String sayHello() {
+        return "index.html";
+    }
+
+}
