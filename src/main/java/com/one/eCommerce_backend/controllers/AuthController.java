84f942ddcf13476611f0f1cef76aa0a578fb0af0
pretend_alphs <abidkasu@gmail.com>
2025-06-18 14:06:27 +0530

    Add JWT authentication filter and logging; implement token validation endpoint

--- src/main/java/com/one/eCommerce_backend/controllers/AuthController.java
+++ src/main/java/com/one/eCommerce_backend/controllers/AuthController.java
@@ -1,13 +1,17 @@
 package com.one.eCommerce_backend.controllers;
 
 import com.one.eCommerce_backend.dtos.JwtResponse;
+import com.one.eCommerce_backend.dtos.UserDto;
 import com.one.eCommerce_backend.dtos.UserLogin;
+import com.one.eCommerce_backend.mappers.UserMapper;
+import com.one.eCommerce_backend.repositories.UserRepository;
 import com.one.eCommerce_backend.services.JwtService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/auth")
@@ -16,6 +20,8 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @PostMapping("/login")
     public ResponseEntity<JwtResponse> login(
@@ -41,5 +47,19 @@ public class AuthController {
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<Void> handleBadCredentialsException() {
         return ResponseEntity.status(401).build();
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var email = (String) authentication.getPrincipal();
+
+        var user = userRepository.findByEmail(email).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        var userDto = userMapper.userToUserDto(user);
+        return ResponseEntity.ok(userDto);
+
     }
 }
